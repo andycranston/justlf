27de24c9150ecbf7b2b798113082db2d47bf964d
andycranston <acranston@gmail.com>
2020-03-06 14:07:58 +0000

    Now uses two temporary files to mitigate against data loss

--- justlf.c
+++ justlf.c
@@ -1,9 +1,11 @@
 /*
- *  @(!--#) @(#) justlf.c, version 002, 05-march-2020
+ *  @(!--#) @(#) justlf.c, version 003, 06-march-2020
  *
  *  remove CR characters (\r) from a text file just leaving LF (\n)
  *  behind.  handy for handling files copied from a Windows system
  *  to a UNIX/Linux one.
+ *
+ *  also make sure last line has a LF (\n) at the end of it
  *
  */
 
@@ -51,8 +53,9 @@ int main(argc, argv)
 {
   char *fname;
   FILE *f;
-  char  tempfname[MAX_FILE_NAME_SIZE + 5];
-  FILE *tempf;
+  char  tempfname1[MAX_FILE_NAME_SIZE + 6];
+  char  tempfname2[MAX_FILE_NAME_SIZE + 6];
+  FILE *tempf1;
   struct stat stbuf;
   int   lc;
   int   c;
@@ -71,12 +74,16 @@ int main(argc, argv)
     exit(1);
   }
 
-  strncpy(tempfname, fname,  MAX_FILE_NAME_SIZE);
-  strncat(tempfname, ".tmp", MAX_FILE_NAME_SIZE + 5);
+  strncpy(tempfname1, fname,   MAX_FILE_NAME_SIZE);
+  strncat(tempfname1, ".tmp1", MAX_FILE_NAME_SIZE + 6);
+
+  strncpy(tempfname2, fname,   MAX_FILE_NAME_SIZE);
+  strncat(tempfname2, ".tmp2", MAX_FILE_NAME_SIZE + 6);
 
   if (debug) {
     printf("[%s]\n", fname);
-    printf("[%s]\n", tempfname);
+    printf("[%s]\n", tempfname1);
+    printf("[%s]\n", tempfname2);
   }
 
   if (stat(fname, &stbuf) != 0) {
@@ -89,15 +96,15 @@ int main(argc, argv)
     exit(1);
   }
 
-  unlink(tempfname);
+  unlink(tempfname1);
 
-  if ((tempf = fopen(tempfname, "w")) == NULL) {
-    fprintf(stderr, "%s: cannot open temporary file \"%s\" for writing\n", progname, tempfname);
+  if ((tempf1 = fopen(tempfname1, "w")) == NULL) {
+    fprintf(stderr, "%s: cannot open temporary file \"%s\" for writing\n", progname, tempfname1);
     exit(1);
   }
 
-  if (chmod(tempfname, stbuf.st_mode) != 0) {
-    fprintf(stderr, "%s: not able to set matching mode on temporary file \"%s\"\n", progname, tempfname);
+  if (chmod(tempfname1, stbuf.st_mode) != 0) {
+    fprintf(stderr, "%s: not able to set matching mode on temporary file \"%s\"\n", progname, tempfname1);
     exit(1);
   }
 
@@ -105,21 +112,36 @@ int main(argc, argv)
 
   while ((c = fgetc(f)) != EOF) {
     if (c != '\r') {
-      fputc(c, tempf);
+      fputc(c, tempf1);
       lc = c;
     }
   }
 
   if (lc != '\n') {
-    fputc('\n', tempf);
+    fputc('\n', tempf1);
   }
 
-  fflush(tempf);
-  fclose(tempf);
+  fflush(tempf1);
+  fclose(tempf1);
 
   fclose(f);
 
-  rename(tempfname, fname);
+  if (rename(fname, tempfname2) != 0) {
+    fprintf(stderr, "%s: unable to rename file \"%s\" to temporary location \"%s\"\n", progname, fname, tempfname2);
+    exit(1);
+  }
+
+  if (rename(tempfname1, fname) != 0) {
+    fprintf(stderr, "%s: unable to rename temporary file \"%s\" to \"%s\"\n", progname, tempfname1, fname);
+    exit(1);
+  }
+
+  if (chmod(fname, stbuf.st_mode) != 0) {
+    fprintf(stderr, "%s: not able to set mode on file \"%s\"\n", progname, fname);
+    exit(1);
+  }
+
+  unlink(tempfname2);
 
   return(0);
 }
